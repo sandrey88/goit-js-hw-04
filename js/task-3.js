21bd31e56fcb685320171dbfcbd8ccbefe212f2a
Andrii <88sushilnikov@gmail.com>
2024-09-02 15:37:50 +0300

    version 1

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,26 @@
-// Задача 3. Фільтрація масиву чисел
+// Задача 3. Профіль гравця
 
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-  // Ітерації по кожному елементу масиву
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
-    }
-  }
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-  return filteredNumbers;
-}
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
